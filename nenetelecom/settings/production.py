57399dbe413207a781d05944d56d9e4a8678ae5a
butadpj <butadpj@gmail.com>
2021-03-06 04:42:51 +0800

    Single static files folder

--- nenetelecom/settings/production.py
+++ nenetelecom/settings/production.py
@@ -4,16 +4,16 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nenetelecom$databasename',
-        'USER': 'nenetelecom',
-        'PASSWORD': 'madeinjapan123',
-        'HOST': 'nenetelecom.mysql.pythonanywhere-services.com',
-        'PORT': ''
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'nenetelecom$databasename',
+#         'USER': 'nenetelecom',
+#         'PASSWORD': 'madeinjapan123',
+#         'HOST': 'nenetelecom.mysql.pythonanywhere-services.com',
+#         'PORT': ''
+#     }
+# }
 
 # INSTALLED_APPS += [
 #     'sslserver'
