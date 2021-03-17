c482bc6961bac411e23bf484c16f5c9e9d26a541
butadpj <butadpj@gmail.com>
2021-03-17 20:55:28 +0800

    Accounts: Improved form design

--- public/DJANGO/js/loginPageForms.js
+++ public/DJANGO/js/loginPageForms.js
@@ -5,13 +5,3 @@ let loginErrorMessage = document.querySelector(".login .error-message p");
 if (loginErrorMessage != null) {
   loginQuote.innerHTML = "";
 }
-
-//Fix issue in mobile phones when pulling up the keyboard
-let loginInputs = document.querySelectorAll(".login-info-form .form-input");
-
-loginInputs.forEach((input) => {
-  input.addEventListener("focus", () => {
-    document.body.style.overflow = "auto";
-    document.body.style.height = "100vh";
-  });
-});
