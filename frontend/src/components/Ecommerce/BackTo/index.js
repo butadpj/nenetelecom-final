8b1845085c1d80684d1b2f61b91ac4540ca6ec0c
butadpj <butadpj@gmail.com>
2021-04-01 18:16:14 +0800

    General Fix based on Previous Added Features

--- frontend/src/components/Ecommerce/BackTo/index.js
+++ frontend/src/components/Ecommerce/BackTo/index.js
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import GetCurrentCustomer from "../../../hooks/GetCurrentCustomer";
+import { getCustomerInfo } from "../../../hooks/query/getCustomerInfo";
 import { CartItemContext } from "../../../context/CartItemContext";
 
 import "./BackTo.css";
 
 const BackTo = ({ linkText, link, icon }) => {
-  const { djangoCurrentUser } = GetCurrentCustomer();
+  const { isAuthenticated } = getCustomerInfo();
   const [state] = useContext(CartItemContext);
   let orders = state.orders;
 
@@ -21,19 +21,21 @@ const BackTo = ({ linkText, link, icon }) => {
             {linkText} {icon}
           </span>
         </div>
-        {djangoCurrentUser === "AnonymousUser" ? (
+        {isAuthenticated ? (
+          orders.length > 0 ? (
+            <div className="view-orders-link">
+              <Link to="/store/my-orders">
+                <h5>View All Orders {`>`}</h5>
+              </Link>
+            </div>
+          ) : null
+        ) : (
           <div className="view-orders-link">
             <a href="/store/accounts/login">
               <h5>Login to save orders {`>`}</h5>
             </a>
           </div>
-        ) : orders.length > 0 ? (
-          <div className="view-orders-link">
-            <Link to="/store/my-orders">
-              <h5>View All Orders {`>`}</h5>
-            </Link>
-          </div>
-        ) : null}
+        )}
       </div>
     </div>
   );
