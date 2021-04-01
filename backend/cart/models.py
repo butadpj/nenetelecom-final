35415646b1b7286d792981b5290d2769492c6a88
butadpj <butadpj@gmail.com>
2021-04-01 17:16:31 +0800

    Backend: Products' Relation to BagItem revert back to set Null

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -46,7 +46,7 @@ class Bag(models.Model):
 
 class BagItem(models.Model):
     bag = models.ForeignKey(Bag, on_delete=models.CASCADE, blank=True, null=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     selected = models.BooleanField(default=True, null=False, blank=False)
 
