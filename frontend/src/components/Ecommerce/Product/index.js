0732d8e6f29f75a51a3ca4157d2266b0c5844900
butadpj <butadpj@gmail.com>
2021-04-18 21:31:56 +0800

    Refactor Product.js: Move any logic from index to --Logic.js

--- frontend/src/components/Ecommerce/Product/index.js
+++ frontend/src/components/Ecommerce/Product/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import "./Product.css";
 
 import defaultImage from "../../../assets/images/Default.png";
@@ -6,129 +6,32 @@ import Button from "../../Button";
 import ProductLogic from "./ProductLogic";
 import Loader from "../../../components/Ecommerce/Loader";
 import ProductDetail from "./ProductDetail";
-import { ProductContext } from "../../../context/ProductContext";
 import InfiniteScroll from "react-infinite-scroll-component";
-import ProductResults from "../InfiniteScrollResults/ProductResults";
 
 const Product = React.memo(() => {
   const {
     handleShowProductDetails,
     handleCloseProductDetails,
-    highlightSearchMatch,
     showDetails,
     detailData,
-  } = ProductLogic();
-
-  const {
-    productResults,
+    productState,
+    loadNextData,
     loading,
     hasMore,
-    offSet,
-    limit,
-    setHasMore,
-    setOffSet,
-    setLoading,
-    setData,
-    shuffle,
-    isRefreshing,
+    productResults,
     setIsRefreshing,
-  } = ProductResults();
-
-  let url = `/api/products/?limit=${limit}&offset=${offSet}`;
-
-  const [productState] = useContext(ProductContext);
-  let products = productState.products;
-  let isLoading = productState.isLoading;
-  let searchInput = productState.productSearchInput;
-  let searchedProductList = productState.searchedProductList;
-  let isSearching = productState.isSearching;
-
-  let searchResult = []; // Matched search will go here
-  // Product Search will happen here
-  useEffect(() => {
-    let input = searchInput.toLowerCase().replace(/\s+/g, " ").trim();
-    products.filter((product) => {
-      let name = product.name.toLowerCase().replace(/\s+/g, " ").trim();
-      let brand = product.brand.toLowerCase().replace(/\s+/g, " ").trim();
-      let condition = product.condition
-        .toLowerCase()
-        .replace(/\s+/g, " ")
-        .trim();
-      let price = `${Number(product.price).toLocaleString()}.00`;
-      if (
-        name.includes(input) ||
-        brand.includes(input) ||
-        condition.includes(input) ||
-        price.includes(input)
-      ) {
-        highlightSearchMatch(input, ".product .product-name");
-        highlightSearchMatch(input, ".product .condition");
-        highlightSearchMatch(input, ".product .product-price .current");
-        searchResult.push(product); // Push it to search result
-      }
-    });
-
-    productState.searchedProductList = searchResult; // Put it on searchedProductList
-  }, [searchInput]);
-
-  useEffect(() => {
-    if (isRefreshing) {
-      setLoading(true);
-      const refreshData = async () => {
-        try {
-          const response = await fetch(url);
-          const data = await response.json();
-          if (!data.next) {
-            setHasMore(false);
-          }
-          setData(shuffle([...productResults, ...data.results]));
-          setLoading(false);
-          setIsRefreshing(false);
-          setOffSet((prev) => {
-            return prev + limit;
-          });
-        } catch (error) {
-          console.error(error);
-          setLoading(false);
-        }
-      };
-      refreshData();
-    }
-  }, [isRefreshing]);
-
-  useEffect(() => {
-    document.body.style.overflow = "auto";
-    loadNextData(url);
-  }, []);
-
-  const loadNextData = async (url) => {
-    console.log("dadsa");
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (!data.next) {
-        setHasMore(false);
-      }
-      setData([...productResults, ...data.results]);
-      setLoading(false);
-      setOffSet((prev) => {
-        return prev + limit;
-      });
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-    }
-  };
+    url,
+  } = ProductLogic();
 
   return (
     <>
-      {isLoading || loading ? (
+      {productState.isLoading || loading ? (
         <Loader />
       ) : // searchedProductList[] will be diplay if user is currently searching
-      isSearching ? (
+      productState.isSearching ? (
         // Check if there's any match for searched product, if none, display "Not found" text
-        searchedProductList.length ? (
-          searchedProductList.map((product) => {
+        productState.searchedProductList.length ? (
+          productState.searchedProductList.map((product) => {
             const {
               id,
               image,
