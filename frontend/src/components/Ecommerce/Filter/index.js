8986d0b8da032e4c7b2dc83245de36d1dc322dc8
butadpj <butadpj@gmail.com>
2021-04-19 19:17:14 +0800

    Ecommerce: Product Filter Feature Started

--- frontend/src/components/Ecommerce/Filter/index.js
+++ frontend/src/components/Ecommerce/Filter/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import "./Filter.css";
 import downBtn from "../../../assets/svgs/caret-down-solid.svg";
 import FilterButton from "./FilterButton";
+import { ProductContext } from "../../../context/ProductContext";
 
 const Filter = () => {
+  const [productState, productDispatch] = useContext(ProductContext);
+  const [filterValue, setFilterValue] = useState("");
+
+  const applyFilter = (value) => {
+    // setFilterValue(value);
+    // productDispatch({
+    //   type: "APPLY_FILTER",
+    //   payload: { input: filterValue },
+    // });
+  };
   return (
     <section className="filter">
       <div className="filter-btns">
         <FilterButton name="price" icon={downBtn} />
         <FilterButton name="category" icon={downBtn} />
         <FilterButton name="condition" icon={downBtn} />
-        <FilterButton name="brand" icon={downBtn} />
+        <FilterButton
+          name="brand"
+          icon={downBtn}
+          functionality={() => applyFilter("Oppo")}
+        />
       </div>
     </section>
   );
